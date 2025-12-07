f5ee26c724a355405bc2b156752093b4854f5740
securmag141-crypto <secur.mag141@gmail.com>
2025-12-07 12:03:29 +0300

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-// firebase-config.js
-const firebaseConfig = {
-   apiKey: "AIzaSyAy60J96o9uzaEx96NQVSFVBNCRxn4NFWY",
-    authDomain: "ssss-7ac80.firebaseapp.com",
-    projectId: "ssss-7ac80",
-    storageBucket: "ssss-7ac80.firebasestorage.app",
-    messagingSenderId: "185019094690",
-    appId: "1:185019094690:web:f75df54462ed848acd29fb"
-};
